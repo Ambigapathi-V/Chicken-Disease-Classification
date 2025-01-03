d63d553a0792c60ffd0b96dd0bc0ef807037e92b
Ambigapath <168761904+Ambigapath@users.noreply.github.com>
2025-01-03 10:00:14 +0530

    Updated Base model Pipeline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.prepare_base_model_pipeline import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -9,3 +10,14 @@ try:
     logger.info(f"{STAGE_NAME} completed successfully.")
 except Exception as e:
     logger.exception(f"{STAGE_NAME} failed. Error: {str(e)}")
+    
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
